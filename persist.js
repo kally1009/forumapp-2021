34a9c8f635397cef810ea8d51a46cd9303b741bd
Kalicia <d00372177@dmail.dixie.edu>
2021-07-02 12:18:47 -0600

    Added to persist and server.js

--- persist.js
+++ persist.js
@@ -1,7 +1,17 @@
 const mongoose = require("mongoose");
 
 function connect(callback){
-    callback();
+    let connectionString = "mongodb+srv://todo_2021:T0d0App@cluster0.5j58h.mongodb.net/forum?retryWrites=true&w=majority";
+    console.log("connect to db...")
+    mongoose
+        .connect(connectionString,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .catch((err)=>{
+            console.log("There was an error connecting to the database")
+        });
+    mongoose.connection.once("open", callback);
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,10 +1,45 @@
 const express = require("express");
 const cors = require("cors");
-const server= express();
+const app= express();
 
 
 
-server.use(cors());
-server.use(express.json({}));
+app.use(cors());
+app.use(express.json({}));
+app.use((req, res, next)=>{
+    console.log("Time:", Date.now(),
+     " - Method:", req.method, 
+     "-Path", req.originalUrl,
+      "-Body: ", req.body);
+    next();
+})
 
-module.exports= server;
+
+//GET /forum
+app.get('/todo',(req, res)=>{
+    res.setHeader("Content-Type","application/json");
+    console.log("Getting Threads");
+    thread (function(err,threads){
+        if(err){
+            console.log(`there was an error listing todos`,err);
+            res.status(500).json({
+                message: `unable to list todos`, error: err 
+            });
+            return;
+        }
+        res.status(200).json(threads);
+    });
+})
+
+
+//GET /forum/:id
+
+//POST /thread
+
+//DELETE /thread/:id
+
+//POST /post
+
+//DELETE /post/:thread_id/:post_id
+
+module.exports= app;
