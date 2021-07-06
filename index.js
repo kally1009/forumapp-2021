b601a33c2e87fc270d03c0c4ebe7bb5ca4b4e6dc
Kalicia Adams <kally1009@kalicia-laptop.localdomain>
2021-07-06 09:51:42 -0600

    heroku updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./server");
 const persist = require("./persist");
 
 
-const port = process.argv[2] || 8080;
+const port = process.argv[2] || process.env.PORT || 8080;
 
 
 persist.connect(()=>{
